fc24a57b8b300eb6c69dda68a2249999e1fdca05
suguru-3u <suguru.13579515@gmail.com>
2021-06-06 16:25:33 +0900

    study equlas

--- src/claenJava/Hero.java
+++ src/claenJava/Hero.java
@@ -1,16 +1,29 @@
 package claenJava;
 
 public class Hero {
-	private String name;
-	private int hp;
+	public String name;
+	public int hp;
 	
-	public Hero(String string, int i) {
-		// TODO 自動生成されたコンストラクター・スタブ
-		this.name = string;
-		this.hp = i;
-	}
+//	public Hero(String string, int i) {
+//		// TODO 自動生成されたコンストラクター・スタブ
+//		this.name = string;
+//		this.hp = i;
+//	}
 
+//	文字列返し
 	public String toString() {
 		return "勇者(名前 = " + this.name + "/HP = " + this.hp + ")";
+	}
+	
+//	等価判定
+	public boolean equals(Object o) {
+		if(o == this) return true;
+		if(o == null) return false;
+		if(!(o instanceof Hero)) return false;
+		Hero r = (Hero)o;
+		if(!this.name.trim().equals(r.name.trim())) {
+			return false;
+		}
+		return true;
 	}
 }
--- src/claenJava/Main.java
+++ src/claenJava/Main.java
@@ -1,11 +1,22 @@
 package claenJava;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Hero a = new Hero("minato",100);
+		Hero a = new Hero();
+		a.name = "minato";
 		System.out.println(a);
+		List<Hero> list = new ArrayList();
+		list.add(a);
+		System.out.println(list.size());
+		a = new Hero();
+		a.name = "minato";
+		list.remove(a);
+		System.out.println(list.size());
 	}
 
 }
