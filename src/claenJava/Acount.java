3635b6eca8fa96868727bf44ce4dcad81f294da1
suguru-3u <suguru.13579515@gmail.com>
2021-06-08 23:37:01 +0900

    study Java

--- src/claenJava/Acount.java
+++ src/claenJava/Acount.java
@@ -1,5 +1,15 @@
 package claenJava;
 
 public class Acount {
-
+	private String acountNo;
+	private int zandaka;
+	private AccountType accountType;
+	
+	public Acount(String aNo,AccountType aType) {
+		
+	}
+	
+	public enum AccountType{
+		FUTUU,TOUZA,TEIKI;
+	}
 }
