cf5d1c84a614b1687c4ecd5bfe7c31e57ca31986
suguru-3u <suguru.13579515@gmail.com>
2021-06-09 21:18:36 +0900

    study sample code

--- src/claenJava/Main.java
+++ src/claenJava/Main.java
@@ -4,21 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Hero a = new Hero();
-//		a.name = "minato";
-//		System.out.println(a);
-//		List<Hero> list = new ArrayList();
-//		list.add(a);
-//		System.out.println(list.size());
-//		a = new Hero();
-//		a.name = "minato";
-//		list.remove(a);
-//		System.out.println(list.size());
-		
-		Pocket<String> p = new Pocket();
-		p.put("1112");
-		String s = p.get();
-		System.out.println(s);
-	}
-
+		String str1 = "文字列1";
+		String str2  = "文字列2";
+		String str = str1 + str2;
+		System.out.println("演算子による文字列の連結->"+ str)	;
+	 }
 }
